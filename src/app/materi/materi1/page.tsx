a9729cec95048f251f675c43e117e529f63d0bc9
Jerannn24 <juniornatra72@gmail.com>
2025-08-19 23:27:50 +0700

    Co-authored-by: update Homepage.tsx, globals.css, page.tsx and MateriBar.tsx Bryant Azraqi Mohammad <Azraqing@users.noreply.github.com>

--- src/app/materi/materi1/page.tsx
+++ src/app/materi/materi1/page.tsx
@@ -1,11 +1,33 @@
-import React, { use } from 'react'
-import Navbar from '@/components/Navbar'
-import MateriBar from '@/components/MateriBar'
+import React from 'react';
+import MateriBar from '@/components/MateriBar';
 
 const page = () => {
     return (
         <div>
             <MateriBar />
+            <div className='mx-25'>
+                <div className='grid grid-cols-5 grid-rows-2 gap-2 m-2' style={{ height: "300px" }}>
+                    <div className='col-span-3 row-span-2 flex flex-col items-center justify-center bg-blue-950'>
+                        <h1 className="font-bold " style={{ fontSize: "50px" }}>Judul Materi</h1>
+                        <h2>Sub-Judul</h2>
+                    </div>
+                    <div className='col-span-2 row-span-2 bg-blue-950 flex items-center justify-center'>
+                        <h1 className="font-bold text-center" style={{ fontSize: "50px" }}>Foto apapun</h1>
+                    </div>
+                </div>
+                <div className='m-2 mt-8'>
+                    <div className='bg-blue-950' style={{ height: "200px", maxWidth: "100%", minWidth: "30%" }}>
+                        <h1>Penjelasan Lagi</h1>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
         </div >
     )
 }
--- src/components/MateriBar.tsx
+++ src/components/MateriBar.tsx
@@ -1,15 +1,39 @@
+"use client";
 import React, { use } from 'react';
 import Navbar from '@/components/Navbar';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const MateriBar = () => {
+    const pathname = usePathname();
     return (
         <div style={{ fontFamily: "var(--font-family)" }}>
             <Navbar />
             <div className='container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-10  mx-auto px-6 py-10'>
-                <div className='text-center font-bold '>Materi 1</div>
-                <div className='text-center font-bold'>Materi 2</div>
-                <div className='text-center font-bold'>Materi 3</div>
-                <div className='text-center font-bold'>Materi 4</div>
+                <div className='text-center font-bold-style'>
+                    <Link href='/materi/materi1' className={`text-center font-bold ${pathname === "/materi/materi1" ? "link-position-materi" : ""
+                        }`}>
+                        Materi 1
+                    </Link>
+                </div>
+                <div className='text-center font-bold materi-style'>
+                    <Link href='/materi/materi2' className={`text-center font-bold ${pathname === "/materi/materi2" ? "link-position-materi" : ""
+                        }`}>
+                        Materi 2
+                    </Link>
+                </div>
+                <div className='text-center font-bold materi-style'>
+                    <Link href='/materi/materi3' className={`text-center font-bold ${pathname === "/materi/materi3" ? "link-position-materi" : ""
+                        }`}>
+                        Materi 3
+                    </Link>
+                </div>
+                <div className='text-center font-bold materi-style'>
+                    <Link href='/materi/materi4' className={`text-center font-bold ${pathname === "/materi/materi4" ? "link-position-materi" : ""
+                        }`}>
+                        Materi 4
+                    </Link>
+                </div>
             </div>
         </div >
     )
