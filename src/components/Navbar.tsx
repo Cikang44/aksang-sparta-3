d64d0280084a7affeea437a66ecfdc8f11763cf7
Jerannn24 <juniornatra72@gmail.com>
2025-08-19 20:28:51 +0700

    Co-authored-by: update globals.css, page.tsx and Navbar.tsx Bryant Azraqi Mohammad <Azraqing@users.noreply.github.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Link from "next/link";
 
-const navbar = () => {
+export default function Navbar() {
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <nav className="shadow-md" style={{ backgroundColor: "var(--navbar-bg)", color: "var(navbar-fg)" }}>
+            <div className="container mx-auto flex items-center justify-between px-6 py-3">
+                {/* Logo */}
+                <Link href="/" className="text-xl font-bold nav-link" style={{ color: "var(navbar-fg)" }}>
+                    Proxy
+                </Link>
 
-export default navbar
+                {/* Menu */}
+                <ul className="hidden md:flex gap-12 font-bold">
+                    <li>
+                        <Link
+                            href="#about"
+                            className="nav-link transition duration-200"
+                        >
+                            Materi
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="#Game"
+                            className="nav-link transition duration-200"
+                        >
+                            Game
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="#Chatbot"
+                            className="nav-link transition duration-200"
+                        >
+                            ChatBot
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="#Inovasi"
+                            className="nav-link transition duration-200"
+                        >
+                            Inovasi
+                        </Link>
+                    </li>
+                </ul>
+
+                {/* Button */}
+                <button className="md:hidden px-3 py-2 rounded bg-blue-500 hover:bg-blue-600">
+                    ☰
+                </button>
+            </div>
+        </nav>
+    );
+}
