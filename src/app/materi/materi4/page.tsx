99600a92964ffd7e041652f9375c5d35f313f70e
Jerannn24 <juniornatra72@gmail.com>
2025-08-23 13:09:35 +0700

    update 34 files

--- src/app/materi/materi4/page.tsx
+++ src/app/materi/materi4/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
                 {/* Bagian atas: 2 kolom pakai flex */}
                 <div className="flex gap-2 m-2 bg-blue-950 rounded-2xl" style={{ height: "300px" }}>
                     <div className="flex flex-col justify-center items-center flex-[3] text-start">
-                        <h1 className="font-bold text-5xl max-w-120">Data Literation:</h1>
+                        <h1 className="font-bold text-5xl max-w-120">Data Literacy: An Ability to Digest Information</h1>
                     </div>
                     <div className="flex items-center justify-center flex-[2] bg-blue-950 text-white rounded-2xl">
                         <img
@@ -19,9 +19,9 @@ const Page = () => {
                         />
                     </div>
                 </div>
-     {/* Bagian tengah (TEKS) */}
-     <div className="m-2 mt-8">
-        <br></br>
+                {/* Bagian tengah (TEKS) */}
+                <div className="m-2 mt-8">
+                    <br></br>
                     <div className="flex flex-col items-start justify-start text-justify">
                         <p className="font-bold text-3xl" style={{ fontSize: "30px" }}>
                             Literasi data adalah kemampuan untuk membaca, menginterpretasikan, dan mengkomunikasikan data sehingga dapat menjadi informasi yang berguna. <br /> <br />
@@ -29,7 +29,7 @@ const Page = () => {
                     </div>
                     <div className="flex flex-col items-center justify-start text-justify">
                         <p className="text-3xl mt-4" style={{ fontSize: "30px" }}>
-                            <b>Masalah + Literasi Data = Solusi<br /></b>                         
+                            <b>Masalah + Literasi Data = Solusi<br /></b>
                         </p>
                     </div>
                     <div className="flex flex-col items-start justify-start text-justify" style={{ minHeight: "200px" }}>
@@ -60,9 +60,9 @@ const Page = () => {
                         <div className="text-3xl mt-8" style={{ fontSize: "30px" }}>
                             <p>
                                 <b>Kompetensi</b> yang diharapkan adalah dapat <b>mengartikulasi</b> masalah yang a, <b>memahami</b> sumber, <b>memastikan </b>
-                                 relevansi, <b>menginterpretasikan</b> wawasan baru, mengambil <b>keputusan</b>, dan kemudian <b>mengkomunikasikannya</b>
+                                relevansi, <b>menginterpretasikan</b> wawasan baru, mengambil <b>keputusan</b>, dan kemudian <b>mengkomunikasikannya</b>
                             </p>
-                        </div>           
+                        </div>
                     </div>
                 </div>
 
@@ -70,12 +70,12 @@ const Page = () => {
                 <div className="m-2 mt-8 flex gap-4 mb-25">
                     <div className=" justify-start text-justify items-start mr-8" style={{ fontSize: "30px", flex: "3" }}>
                         <p> Sebagai manusia pasti memiliki data. Data digital adalah yang sering kita temui seperti, </p>
-                            <ol className="list-decimal text-3xl ml-10">
-                                <li><b>Riwayat Digital</b>: klik situs web, penggunaan aplikasi, belanja online, data lokasi, dan  interaksi media sosial.</li>
-                                <li><b>Rekam medis</b>: data kesehatan pasien yang terekam sepanjang hidup.</li>
-                                <li><b>Data citra dan video</b>: hasil rekaman kamera, tangkapan layar, video CCTV.</li>
-                                <li><b>Data otomotif</b>: data pengemudi, perilaku berkendara, dan data pabrik kendaraan</li>
-                            </ol>
+                        <ol className="list-decimal text-3xl ml-10">
+                            <li><b>Riwayat Digital</b>: klik situs web, penggunaan aplikasi, belanja online, data lokasi, dan  interaksi media sosial.</li>
+                            <li><b>Rekam medis</b>: data kesehatan pasien yang terekam sepanjang hidup.</li>
+                            <li><b>Data citra dan video</b>: hasil rekaman kamera, tangkapan layar, video CCTV.</li>
+                            <li><b>Data otomotif</b>: data pengemudi, perilaku berkendara, dan data pabrik kendaraan</li>
+                        </ol>
                         <p className="text-3xl mt-8">
                             <b>Q</b>: Mengapa dibutuhkan literasi data? <br />
                             <b>A</b>: Diperlukan untuk meningkatkan kualitas layanan, mempercepat pengambilan keputusan, meraih keunggulan kompetitif, membuat prediksi akurat, dan mengelola risiko.
@@ -86,7 +86,7 @@ const Page = () => {
                             <h1 className='font-bold' style={{ fontSize: "50px" }}>Foto</h1>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
